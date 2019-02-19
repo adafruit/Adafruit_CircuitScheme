d122591e278e057ff533af2d2aa4343b78f84881
Dave Astels <dastels@daveastels.com>
2019-02-19 16:03:29 -0500

    Start a file of misc functions and macros

---
+++ lisp/misc.scm
@@ -0,0 +1,16 @@
+;;; Scheme Interpreter in CircuitPython
+;;; Based on Lispy.py (c) Peter Norvig, 2010; See http://norvig.com/lispy2.html
+;;;
+;;; Adafruit invests time and resources providing this open source code.
+;;; Please support Adafruit and open source hardware by purchasing
+;;; products from Adafruit!
+;;;
+;;; Written by Dave Astels for Adafruit Industries
+;;; Copyright (c) 2019 Adafruit Industries
+;;; Licensed under the MIT license.
+;;;
+;;; All text above must be included in any redistribution.
+;;;
+;;; Misc functions
+
+(define false? not)
